6aa942262529cae6a4fe48054dd3eecce11326db
Anthony Quinones <quinoant@gmail.com>
2023-06-26 14:01:34 -0500

    angular

--- journals/src/main/java/com/voxpopuli/journals/journals/Models/Journal.java
+++ journals/src/main/java/com/voxpopuli/journals/journals/Models/Journal.java
@@ -4,6 +4,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
+/**
+ * {
+        "entryDate": "3-2-1999",
+        "releaseDate": "2-2-2025",
+        "content": "Going on a cruise!"
+    }
+    How an entry for this looks
+    Simple Entity class that is controlled by User controller
+    all of these are anonomous to everyone but the user that
+    posted them
+ */
 @Entity // This tells Hibernate to make a table out of this class
 public class Journal {
     @Id
@@ -15,6 +27,9 @@ public class Journal {
     private String releaseDate;
 
     private String content;
+    public Integer getJournalID(){
+        return journalID;
+    }
 
     public String getEntryDate() {
         return entryDate;
--- journals/src/main/java/com/voxpopuli/journals/user/Controllers/UsersController.java
+++ journals/src/main/java/com/voxpopuli/journals/user/Controllers/UsersController.java
@@ -30,27 +30,50 @@ public class UsersController {
     @Autowired
     private UserRepository userRepository;
 
+    /**
+     * lists all users
+     * @return - all users
+     */
     @GetMapping
     public List<User> list(){
         return userRepository.findAll();
     }
 
+    /**
+     * Gets all journals anonymousized
+     * @return - every journal
+     */
     @GetMapping("/allJournals")
     public List<Journal> get(){
         return journalRepository.findAll();
     }
 
+    /**
+     * Makes a user
+     * @param user - the request body to make that user
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
     public void create(@RequestBody User user){
         userRepository.save(user);
     }
 
+    /**
+     * Gets info about a specific user
+     * @param id
+     * @return - that specific user obj
+     */
     @GetMapping("/{id}")
     public User get(@PathVariable("id") long id){
         return userRepository.getReferenceById(id);
     }
 
+    /**
+     * Adds a journal to a user, since journals as of now can only be added
+     * to users this works great
+     * @param id
+     * @param journal
+     */
     @PostMapping("/addJournal/{id}")
     @ResponseStatus(HttpStatus.OK)
     public void addJournal(@PathVariable("id") long id,@RequestBody Journal journal){
@@ -59,166 +82,25 @@ public class UsersController {
         journalRepository.save(journal);
     }
 
+    /**
+     * Adds a anonymous jounal
+     * @param journal - journal to add
+     */
+    @PostMapping("/addAnonJournal")
+    @ResponseStatus(HttpStatus.OK)
+    public void addAnonymousJournal(@RequestBody Journal journal){
+        journalRepository.save(journal);
+    }
+
+    /**
+     * Gets all of a specific users journals
+     * @param id - a users specific id number
+     * @return all of a users journals
+     */
     @GetMapping("/getJournals/{id}")
     public Set<Journal> getUserJournals(@PathVariable("id") long id){
         User user =  userRepository.getReferenceById(id);
         return user.getJournals();
     }
-    // /**
-    //  * This method is used to register a user
-    //  * @param username
-    //  * @param password
-    //  * @param firstName
-    //  * @param lastName
-    //  * @return HttpStatus.OK if the user was successfully registered,
-    //  *          HttpStatus.CONFLICT if the username already exists
-    //  */
-    // @GetMapping(path="/register/{username}/{password}/{firstName}/{lastName}")
-    // public @ResponseBody String register(@PathVariable("username") String username, @PathVariable("password") String password, @PathVariable("firstName") String firstName, @PathVariable("lastName") String lastName){
-    //     Iterable<User> userRepo = getAllUsers();
-    //     for (User user : userRepo) {
-    //         if (user.getUsername().equals(username))
-    //             return "user_exists";
-    //     }
-
-    //     User newUser = new User();
-    //     newUser.setUsername(username);
-    //     newUser.setPassword(password);
-    //     newUser.setFirstName(firstName);
-    //     newUser.setLastName(lastName);
-    //     userRepository.save(newUser);
-    //     return "success";
-    // }
-
-    // /**
-    //  * This method is used to login a user
-    //  * @param username
-    //  * @param password
-    //  * @return HttpStatus.OK if the user exists and the password is correct,
-    //  *          HttpStatus.NOT_FOUND if the user does not exist,
-    //  *          HttpStatus.UNAUTHORIZED if the password is incorrect
-    //  */
-    // @GetMapping(path="/login/{username}/{password}")
-    // public @ResponseBody String login(@PathVariable String username, @PathVariable String password){
-    //     Iterable<User> userRepo = getAllUsers();
-    //     for (User user : userRepo) {
-    //         if(user.getUsername().equals(username)){
-    //             if(user.getPassword().equals(password))
-    //                 return "success";
-    //             else
-    //                 return "wrong_password";
-    //         }
-    //     }
-    //     return "userDNE";
-    // }
-
-    // /**
-    //  * Change a users username
-    //  * 
-    //  * @param oldUsername The users current username
-    //  * @param newUsername The users new username
-    //  * @return A string telling if it was a successful change or that the username does not exist
-    //  */
-    // @GetMapping(path="/username/{oldUsername}/{newUsername}")
-    // public @ResponseBody String changeUsername(@PathVariable("oldUsername") String oldUsername, @PathVariable("newUsername") String newUsername){
-    //     Iterable<User> userRepo = getAllUsers();
-    //     for (User user : userRepo) {
-    //         if (user.getUsername().equals(oldUsername)){
-    //             user.setUsername(newUsername);
-    //             userRepository.save(user);
-    //             return "Username successfully changed!";
-    //         }
-    //     }
-    //     return "That username does not exist.";
-    // }
-
-    // /**
-    //  * Change a users password
-    //  * 
-    //  * @param username The users current username
-    //  * @param newPassword The users new password
-    //  * @return A string telling if it was a successful change or that the username does not exist
-    //  */
-    // @GetMapping(path="/password/{username}/{newPassword}")
-    // public @ResponseBody String changePassowrd(@PathVariable("username") String username, @PathVariable("newPassword") String newPassword){
-    //     Iterable<User> userRepo = getAllUsers();
-    //     for (User user : userRepo) {
-    //         if (user.getUsername().equals(username)){
-    //             user.setPassword(newPassword);
-    //             userRepository.save(user);
-    //             return "Password successfully changed!";
-    //         }
-    //     }
-    //     return "That username does not exist.";
-    // }
-
-    // /**
-    //  * Change a users First Name
-    //  * 
-    //  * @param username The users current username
-    //  * @param newFirstName The users new password
-    //  * @return A string telling if it was a successful change or that the username does not exist
-    //  */
-    // @GetMapping(path="/firstname/{username}/{newFirstName}")
-    // public @ResponseBody String changeFirstName(@PathVariable("username") String username, @PathVariable("newFirstName") String newFirstName){
-    //     Iterable<User> userRepo = getAllUsers();
-    //     for (User user : userRepo) {
-    //         if (user.getUsername().equals(username)){
-    //             user.setFirstName(newFirstName);
-    //             userRepository.save(user);
-    //             return "First Name successfully changed!";
-    //         }
-    //     }
-    //     return "That username does not exist.";
-    // }
-
-    // /**
-    //  * Change a users Last name
-    //  * 
-    //  * @param username The users current username
-    //  * @param newLastName The users new password
-    //  * @return A string telling if it was a successful change or that the username does not exist
-    //  */
-    // @GetMapping(path="/lastname/{username}/{newFirstName}")
-    // public @ResponseBody String changeLastName(@PathVariable("username") String username, @PathVariable("newLastName") String newLastName){
-    //     Iterable<User> userRepo = getAllUsers();
-    //     for (User user : userRepo) {
-    //         if (user.getUsername().equals(username)){
-    //             user.setLastName(newLastName);
-    //             userRepository.save(user);
-    //             return "Last Name successfully changed!";
-    //         }
-    //     }
-    //     return "That username does not exist.";
-    // }
-
-    // /**
-    //  * remove a user
-    //  * 
-    //  * @param username The users current username
-    //  * @return A string telling if it was a successful remove or that the username does not exist
-    //  */
-    // @GetMapping(path="/remove/{username}")
-    // public @ResponseBody String removeUser(@PathVariable("username") String username){
-    //     Iterable<User> userRepo = getAllUsers();
-    //     for (User user : userRepo) {
-    //         if (user.getUsername().equals(username)){
-    //             userRepository.delete(user);
-    //             return "User successfully removed!";
-    //         }
-    //     }
-    //     return "That username does not exist.";
-    // }
-
-    
-
-    // public UserRepository getRepo(){
-    //     return this.userRepository;
-    // }
-
-    // @GetMapping
-    // public @ResponseBody Iterable<User> getAllUsers() {
-    //     return this.userRepository.findAll();
-    // }
 }
 
